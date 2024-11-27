55b9a17e2b8bcacbb7e02b4c73c32fa8e1a262d4
Kevin <q.hu@mail.utoronto.ca>
2024-11-27 02:33:27 -0500

    Fixed avatar display with non-square aspect ratio

--- PP2/src/components/nav-user.tsx
+++ PP2/src/components/nav-user.tsx
@@ -73,7 +73,7 @@ export function NavUser() {
               className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
             >
               <Avatar className="h-8 w-8 rounded-lg">
-                <AvatarImage src={user.avatar} alt={user.firstName + " " + user.lastName} />
+                <AvatarImage src={user.avatar} alt={user.firstName + " " + user.lastName} className='object-cover' />
                 <AvatarFallback className="rounded-lg">
                   {user.firstName + user.lastName}
                 </AvatarFallback>
@@ -94,7 +94,7 @@ export function NavUser() {
             <DropdownMenuLabel className="p-0 font-normal">
               <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
                 <Avatar className="h-8 w-8 rounded-lg">
-                  <AvatarImage src={user.avatar} alt={user.firstName + " " + user.lastName} />
+                  <AvatarImage src={user.avatar} alt={user.firstName + " " + user.lastName} className='object-cover' />
                   <AvatarFallback className="rounded-lg">
                     {user.firstName + user.lastName}
                   </AvatarFallback>
